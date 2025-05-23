2c6824988059ba26699fe588f6dcf9c4a2705b17
sunyesle <45172865+sunyesle@users.noreply.github.com>
2025-05-23 16:15:34 +0900

    Product 목록 조회 API 추가

--- product-service/src/main/java/com/sunyesle/product_service/ProductService.java
+++ product-service/src/main/java/com/sunyesle/product_service/ProductService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -19,5 +21,11 @@ public class ProductService {
         productRepository.save(product);
         log.info("Product created successfully");
         return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll().stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
     }
 }
