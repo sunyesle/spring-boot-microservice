f74ccf1c9116fd34a2f3c5084eda377113a94339
sunyesle <45172865+sunyesle@users.noreply.github.com>
2025-05-23 16:15:11 +0900

    Product API 엔드포인트 수정

--- product-service/src/main/java/com/sunyesle/product_service/ProductController.java
+++ product-service/src/main/java/com/sunyesle/product_service/ProductController.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
@@ -14,5 +16,10 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
         return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
