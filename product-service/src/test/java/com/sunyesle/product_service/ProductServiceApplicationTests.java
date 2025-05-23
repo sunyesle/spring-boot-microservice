8bfa202684d6a49cf331b861a717c6dd1e5b3475
sunyesle <45172865+sunyesle@users.noreply.github.com>
2025-05-23 16:54:19 +0900

    테스트 환경 설정

--- product-service/src/test/java/com/sunyesle/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/sunyesle/product_service/ProductServiceApplicationTests.java
@@ -1,15 +1,26 @@
 package com.sunyesle.product_service;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
 
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
