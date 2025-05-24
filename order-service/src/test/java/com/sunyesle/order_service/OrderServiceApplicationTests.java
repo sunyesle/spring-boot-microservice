73dbc745268086898211f5cc60c5d39457a348a8
sunyesle <sunyesle@gmail.com>
2025-05-24 23:40:04 +0900

    order-service 테스트 환경 설정

--- order-service/src/test/java/com/sunyesle/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/sunyesle/order_service/OrderServiceApplicationTests.java
@@ -1,12 +1,24 @@
 package com.sunyesle.order_service;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
 
 	@Test
 	void contextLoads() {
