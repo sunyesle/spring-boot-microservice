01567de9bf0cc3d20cb023b3bece0ebeed388caf
sunyesle <sunyesle@gmail.com>
2025-05-25 17:17:20 +0900

    상품 재고 여부 확인 API 테스트 추가

--- inventory-service/src/test/java/com/sunyesle/inventory_service/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/sunyesle/inventory_service/InventoryServiceApplicationTests.java
@@ -1,15 +1,48 @@
 package com.sunyesle.inventory_service;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apiguardian.api.API;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
 
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void 상품_재고_여부를_확인한다() {
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .param("skuCode", "AA000000")
+                .param("quantity", 1)
+                .when()
+                .get("/api/inventory")
+                .then()
+                .statusCode(200)
+                .body(Matchers.equalTo("true"));
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .param("skuCode", "AA000000")
+                .param("quantity", 1000)
+                .when()
+                .get("/api/inventory")
+                .then()
+                .statusCode(200)
+                .body(Matchers.equalTo("false"));
+    }
 }
