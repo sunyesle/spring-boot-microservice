88de1a5061f5d059f743593692ed822b6d9fb2d1
sunyesle <sunyesle@gmail.com>
2025-05-27 01:47:01 +0900

    API Gateway에 Keycloak 기반 OAuth2 리소스 서버 설정 추가

---
+++ api-gateway/src/main/java/com/sunyesle/api_gateway/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.sunyesle.api_gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
