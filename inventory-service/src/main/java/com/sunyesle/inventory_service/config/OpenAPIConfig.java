42076136fedddf760a3fce08537766d6bf11d1eb
sunyesle <sunyesle@gmail.com>
2025-05-28 21:03:45 +0900

    모든 서비스에 CORS 설정 추가

--- inventory-service/src/main/java/com/sunyesle/inventory_service/config/OpenAPIConfig.java
+++ inventory-service/src/main/java/com/sunyesle/inventory_service/config/OpenAPIConfig.java
@@ -1,4 +1,4 @@
-package com.sunyesle.inventory_service;
+package com.sunyesle.inventory_service.config;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
--- product-service/src/main/java/com/sunyesle/product_service/config/OpenAPIConfig.java
+++ product-service/src/main/java/com/sunyesle/product_service/config/OpenAPIConfig.java
@@ -1,4 +1,4 @@
-package com.sunyesle.order_service;
+package com.sunyesle.product_service.config;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -11,14 +11,14 @@ import org.springframework.context.annotation.Configuration;
 public class OpenAPIConfig {
 
     @Bean
-    public OpenAPI orderServiceAPI() {
+    public OpenAPI productServiceAPI() {
         return new OpenAPI()
-                .info(new Info().title("Order Service API")
-                        .description("This is the REST API for Order Service")
+                .info(new Info().title("Product Service API")
+                        .description("This is the REST API for Product Service")
                         .version("v0.0.1")
                         .license(new License().name("Apache 2.0")))
                 .externalDocs(new ExternalDocumentation()
-                        .description("You can refer to the Order Service Wiki Documentation")
-                        .url("https://order-service-dummy-url.com/docs"));
+                        .description("You can refer to the Product Service Wiki Documentation")
+                        .url("https://product-service-dummy-url.com/docs"));
     }
 }
