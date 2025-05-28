3608d591d5521e4413641671a82f9d34fd049118
sunyesle <sunyesle@gmail.com>
2025-05-28 20:37:10 +0900

    API Gateway에 OpenAPI 적용 및 설정

--- api-gateway/src/main/java/com/sunyesle/api_gateway/SecurityConfig.java
+++ api-gateway/src/main/java/com/sunyesle/api_gateway/SecurityConfig.java
@@ -9,10 +9,14 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(freeResourceUrls).permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
