76ae8ffcbe7a6f776f24914e906e50670c5c6ca9
sunyesle <sunyesle@gmail.com>
2025-06-01 12:39:38 +0900

    OpenFeign에서 HTTP Interface로 변경

--- order-service/src/main/java/com/sunyesle/order_service/InventoryClient.java
+++ order-service/src/main/java/com/sunyesle/order_service/InventoryClient.java
@@ -1,12 +1,10 @@
 package com.sunyesle.order_service;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(name = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
 
-    @GetMapping("/api/inventory")
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
---
+++ order-service/src/main/java/com/sunyesle/order_service/config/RestClientConfig.java
@@ -0,0 +1,26 @@
+package com.sunyesle.order_service.config;
+
+import com.sunyesle.order_service.InventoryClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${inventory.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public InventoryClient inventoryClient() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+        RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+}
