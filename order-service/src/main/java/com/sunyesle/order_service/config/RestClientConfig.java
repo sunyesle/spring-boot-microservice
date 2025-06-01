c074e0072779da6f6eaf9e3cdea1c26f85452209
sunyesle <sunyesle@gmail.com>
2025-06-01 15:27:00 +0900

    Apache HttpClient 5 기반 RestClient 설정

--- order-service/src/main/java/com/sunyesle/order_service/config/RestClientConfig.java
+++ order-service/src/main/java/com/sunyesle/order_service/config/RestClientConfig.java
@@ -1,9 +1,17 @@
 package com.sunyesle.order_service.config;
 
 import com.sunyesle.order_service.InventoryClient;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.TimeValue;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -18,9 +26,31 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        // 커넥션 풀 설정
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(200); // 전체 최대 커넥션 수
+        connectionManager.setDefaultMaxPerRoute(50); // 라우트당 최대 커넥션 수
+
+        // 타임아웃 설정
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofSeconds(2)) // 연결 타임아웃
+                .setResponseTimeout(Timeout.ofSeconds(5)) // 읽기 타임아웃
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .evictIdleConnections(TimeValue.ofSeconds(30)) // 유휴 커넥션 정리
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
     }
 }
