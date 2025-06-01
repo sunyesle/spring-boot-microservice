e16674700bacb6ffac67e58cc7adc05d4efdad85
sunyesle <sunyesle@gmail.com>
2025-06-01 12:10:18 +0900

    하드코딩되어있던 URL을 프로퍼티 파일을 통해 주입받도록 수정

--- order-service/src/main/java/com/sunyesle/order_service/InventoryClient.java
+++ order-service/src/main/java/com/sunyesle/order_service/InventoryClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "inventory", url = "http://localhost:8082")
+@FeignClient(name = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
 
     @GetMapping("/api/inventory")
