56c71f97c639995adbe476d8130209af804ff835
sunyesle <sunyesle@gmail.com>
2025-06-04 20:04:35 +0900

    preflight 요청에 대해 401 응답을 반환하는 오류 수정

--- api-gateway/src/main/java/com/sunyesle/api_gateway/SecurityConfig.java
+++ api-gateway/src/main/java/com/sunyesle/api_gateway/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.sunyesle.api_gateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -16,6 +17,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
                         .pathMatchers(freeResourceUrls).permitAll()
                         .anyExchange().authenticated()
                 )
