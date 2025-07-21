772d432c16ceeb23ba62d0a9ad93272e2f80ca9f
sunyesle <sunyesle@gmail.com>
2025-07-21 19:56:22 +0900

    상품 재고 생성 API 테스트 추가

--- inventory-service/src/test/java/com/sunyesle/inventory_service/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/sunyesle/inventory_service/InventoryServiceApplicationTests.java
@@ -2,7 +2,6 @@ package com.sunyesle.inventory_service;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.apiguardian.api.API;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,6 +20,27 @@ class InventoryServiceApplicationTests {
     void setUp() {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = port;
+    }
+
+    @Test
+    void 상품_재고를_생성한다() {
+        String requestBody = """
+                {
+                    "skuCode": "ZZ000000",
+                    "quantity": 1000
+                }
+                """;
+
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/api/inventory")
+                .then()
+                .statusCode(201)
+                .body("id", response -> Matchers.notNullValue())
+                .body("skuCode", response -> Matchers.equalTo("ZZ000000"))
+                .body("quantity", response -> Matchers.equalTo(1000));
     }
 
     @Test
