87f1e23f0804e8b24bc626ef69425132dce19b68
sunyesle <sunyesle@gmail.com>
2026-02-28 03:08:31 +0900

    주문 생성 테스트 수정

--- order-service/src/test/java/com/sunyesle/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/sunyesle/order_service/OrderServiceApplicationTests.java
@@ -18,63 +18,77 @@ import org.springframework.test.context.ActiveProfiles;
 @AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
-	@LocalServerPort
-	private int port;
+    @LocalServerPort
+    private int port;
 
-	@BeforeEach
-	void setUp() {
-		RestAssured.baseURI = "http://localhost";
-		RestAssured.port = port;
-	}
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
 
-	@Test
-	void 주문을_생성한다() {
-		String requestBody = """
-				{
-				    "skuCode": "AA000000",
-				    "price": 89000,
-				    "quantity": 1,
-				    "userDetails": {
-				        "email": "test@gmail.com",
-				        "firstName": "John",
-				        "lastName": "Doe"
-				    }
-				}
-				""";
-		InventoryClientStub.stubInventoryCall("AA000000", 1, true);
+    @Test
+    void 주문을_생성한다() {
+        String requestBody = """
+                {
+                    "skuCode": "AA000000",
+                    "price": 89000,
+                    "quantity": 1,
+                    "userDetails": {
+                        "email": "test@gmail.com",
+                        "firstName": "John",
+                        "lastName": "Doe"
+                    }
+                }
+                """;
+        String response = """
+                {
+                  "id": 1,
+                  "skuCode": "AA000000",
+                  "quantity": 100
+                }
+                """;
+        InventoryClientStub.stubInventoryGetStockCall("AA000000", response);
 
-		RestAssured.given()
-				.contentType(ContentType.JSON)
-				.body(requestBody)
-				.when()
-				.post("/api/orders")
-				.then()
-				.statusCode(201)
-				.body(Matchers.equalTo("Order Placed Successfully"));
-	}
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/api/orders")
+                .then()
+                .statusCode(201)
+                .body(Matchers.equalTo("Order Placed Successfully"));
+    }
 
-	@Test
-	void 재고가_부족할_경우_주문_생성에_실패한다() {
-		String requestBody = """
-				{
-				    "skuCode": "AA000000",
-				    "price": 89000,
-				    "quantity": 1,
-				    "userDetails": {
-				        "email": "test@gmail.com",
-				        "firstName": "John",
-				        "lastName": "Doe"
-				    }
-				}
-				""";
-		InventoryClientStub.stubInventoryCall("AA000000", 1, false);
+    @Test
+    void 재고가_부족할_경우_주문_생성에_실패한다() {
+        String requestBody = """
+                {
+                    "skuCode": "AA000000",
+                    "price": 89000,
+                    "quantity": 1,
+                    "userDetails": {
+                        "email": "test@gmail.com",
+                        "firstName": "John",
+                        "lastName": "Doe"
+                    }
+                }
+                """;
+        String response = """
+                {
+                  "id": 1,
+                  "skuCode": "AA000000",
+                  "quantity": 0
+                }
+                """;
+        InventoryClientStub.stubInventoryGetStockCall("AA000000", response);
 
-		RestAssured.given()
-				.contentType(ContentType.JSON)
-				.body(requestBody)
-				.when()
-				.post("/api/orders")
-				.then()
-				.statusCode(500);
-	}
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .when()
+                .post("/api/orders")
+                .then()
+                .statusCode(500);
+    }
 }
--- order-service/src/test/java/com/sunyesle/order_service/stub/InventoryClientStub.java
+++ order-service/src/test/java/com/sunyesle/order_service/stub/InventoryClientStub.java
@@ -4,11 +4,19 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 public class InventoryClientStub {
 
-    static public void stubInventoryCall(String skuCode, Integer quantity, boolean responseBody) {
+    static public void stubInventoryIsInStockCall(String skuCode, Integer quantity, boolean responseBody) {
         stubFor(get(urlEqualTo("/api/inventory?skuCode=" + skuCode + "&quantity=" + quantity))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
                         .withBody(String.valueOf(responseBody))));
+    }
+
+    static public void stubInventoryGetStockCall(String skuCode, String responseBody) {
+        stubFor(get(urlEqualTo("/api/inventory/" + skuCode))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(responseBody)));
     }
 }
